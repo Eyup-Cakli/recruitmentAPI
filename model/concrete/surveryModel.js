bdd753843dbd17a37c6b956e988cc5602d5ef9e5
Eyup Cakli <eyupcakli@hotmail.com>
2024-03-22 15:02:49 +0300

    feat(model): surveryModel mongodb schema created.

---
+++ model/concrete/surveryModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const projectBaseSchema = require("../projectBaseSchema.js");
+
+const surverySchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, "You must be fill in this field."],
+        maxlength: [24, "You have exceeded the maximum number of characters."]
+    },
+    description: {
+        type: String,
+        required: [true, "You must be fill in this field."],
+        maxlength: [65, "You have exceeded the maximum number of characters."]
+    },
+    ...projectBaseSchema
+});
+
+const surveryModel = mongoose.model("survery", surverySchema);
+module.exports = surveryModel;
