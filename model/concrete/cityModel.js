58d1b94722e82724f52219804bc9ca2892065428
Eyup Cakli <eyupcakli@hotmail.com>
2024-03-22 14:56:42 +0300

    feat&refactor(model): cityModel mongodb schema created. model folder name modified.

---
+++ model/concrete/cityModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const projectBaseSchema = require("../projectBaseSchema.js");
+
+const citySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "You must be fill in this field."],
+        maxlength: [24, "You have exceeded the maximum number of characters."]
+    },
+    ...projectBaseSchema
+});
+
+const cityModel = mongoose.model("city", citySchema);
+module.exports = cityModel;
