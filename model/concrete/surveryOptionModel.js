16c79181099a43b3ad99c83ecd22d30d65cdc1a4
Eyup Cakli <eyupcakli@hotmail.com>
2024-03-22 16:25:16 +0300

    feat(model): surveryOptionModel mongodb schema created.

---
+++ model/concrete/surveryOptionModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const projectBaseSchema = require("../projectBaseSchema.js");
+
+const surveryOptionSchema = new mongoose.Schema({
+    questionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "surveryQuestions",
+        required: [true, "You must be fill in this field."]
+    },
+    optionText: {
+        type: String,
+        maxlength: [50, "You have exceeded the maximum number of characters."],
+        required: [true, "You must be fill in this field."]
+    },
+    ...projectBaseSchema
+});
+
+const surveryOptionModel = new mongoose.model("surveryOptions", surveryOptionSchema);
+module.exports = surveryOptionModel;
